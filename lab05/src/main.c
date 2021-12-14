1fb4ee8ac40523736034d98b0dd24b60a48414cb
mishabreslavets <91278383+mishabreslavets@users.noreply.github.com>
2021-12-14 12:13:23 +0200

    comments added

--- lab05/src/main.c
+++ lab05/src/main.c
@@ -1,10 +1,10 @@
 #include <stdbool.h>
 
 int main(){
-	int input_number = 37;
-	bool res = false;
-	for(int i = 2; i*i <= input_number; i++){
-		if (input_number%i == 0){
+	int input_number = 37; //введення числа на перевірку
+	bool res = false; //змінна що буде відповідати за результат
+	for(int i = 2; i*i <= input_number; i++){ 
+		if (input_number%i == 0){ //перевірка на цілочисленне ділення тобто складенність
 			res = true;
 		}
 	}
